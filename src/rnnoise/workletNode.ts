e290c0fe41a18024b1542fd2965d780f3446432f
sapphi-red <green@sapphi.red>
2022-02-04 21:21:40 +0900

    Add RnnoiseWorkletNode comment

--- src/rnnoise/workletNode.ts
+++ src/rnnoise/workletNode.ts
@@ -1,6 +1,9 @@
 import { type RnnoiseProcessorOptions } from './options'
 import { id, type RnnoiseWorkletOptions } from './workletUtil'
 
+/**
+ * Assumes sample rate to be 48kHz.
+ */
 export class RnnoiseWorkletNode extends AudioWorkletNode {
   constructor(
     context: AudioContext,
