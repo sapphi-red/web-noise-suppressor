1b814bb6a98c02f76bdf6733f11f50838ab7d7b7
sapphi-red <green@sapphi.red>
2022-02-04 16:58:35 +0900

    Delete wrapAudioBufferAsArray

--- src/utils/audioBufferWrapper.ts
+++
@@ -1,9 +0,0 @@
-export const wrapAudioBufferAsArray = (buff: AudioBuffer) =>
-  new Proxy(buff, {
-    get(target, p, receiver) {
-      if (typeof p === 'string' && parseInt(p, 10) + '' === p) {
-        return target.getChannelData(+p)
-      }
-      return Reflect.get(target, p, receiver)
-    }
-  }) as unknown as ArrayLike<Float32Array>
