0226cc6d2fc42b97ef0a7e6db522614a2d267daf
sapphi-red <green@sapphi.red>
2022-02-04 19:15:37 +0900

    Fix demo

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -12,12 +12,17 @@ import { setupVisualizer } from './visualizer'
 
 //
 ;(async () => {
+  const ctx = new AudioContext()
+
   console.log('1: Setup...')
   const speexWasmBinary = await loadSpeex({ url: '/wasms/speex.wasm' })
   const rnnoiseWasmBinary = await loadRnnoise({
     url: '/wasms/rnnoise.wasm',
     simdUrl: '/wasms/rnnoise_simd.wasm'
   })
+  await ctx.audioWorklet.addModule(speexWorkletPath)
+  await ctx.audioWorklet.addModule(noiseGateWorkletPath)
+  await ctx.audioWorklet.addModule(rnnoiseWorkletPath)
   console.log('1: Setup done')
 
   const $startButton = document.getElementById(
@@ -26,33 +31,16 @@ import { setupVisualizer } from './visualizer'
   const $form = document.getElementById('form') as HTMLFormElement
 
   const $canvas = document.getElementById('canvas') as HTMLCanvasElement
+  const analyzer = setupVisualizer($canvas, ctx)
 
-  let moduleAdded = false
   let speex: SpeexWorkletNode | undefined
   let rnnoise: RnnoiseWorkletNode | undefined
   let noiseGate: NoiseGateWorkletNode | undefined
   let gain: GainNode | undefined
-  let analyzer: AnalyserNode | undefined
   $form.addEventListener('submit', async e => {
     e.preventDefault()
     $startButton.disabled = true
-
-    const ctx = new AudioContext()
-    if (ctx.state === 'suspended') {
-      ctx.resume()
-    }
-
-    if (!moduleAdded) {
-      moduleAdded = true
-
-      await ctx.audioWorklet.addModule(speexWorkletPath)
-      await ctx.audioWorklet.addModule(noiseGateWorkletPath)
-      await ctx.audioWorklet.addModule(rnnoiseWorkletPath)
-    }
-
-    if (!analyzer) {
-      analyzer = setupVisualizer($canvas, ctx)
-    }
+    ctx.resume()
 
     const formData = new FormData($form)
     const type = formData.get('type')
