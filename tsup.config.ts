94924d042e81c982ba7ea3d2ffb8842744dd64c2
sapphi-red <green@sapphi.red>
2022-10-30 17:21:20 +0900

    feat: better build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,34 @@
 import { defineConfig } from 'tsup'
+import type { Options } from 'tsup'
 
-export default defineConfig({
-  entry: [
-    'src/index.ts',
-    'src/speex/workletProcessor.ts',
-    'src/noiseGate/workletProcessor.ts',
-    'src/rnnoise/workletProcessor.ts'
-  ],
-  target: 'es2020',
-  platform: 'browser',
-  format: ['esm', 'cjs'],
-  dts: true,
-  sourcemap: true,
-  splitting: false, // to prevent "import" inside worklet which breaks vite
-  external: ['fs', 'path'],
-  define: {
-    window: '{}' // to be detected as ENVIRONMENT_IS_WEB with EmscriptenModule
+export default defineConfig(() => {
+  const common: Options = {
+    target: 'es2020',
+    platform: 'browser',
+    dts: true,
+    sourcemap: true,
+    external: ['fs', 'path'],
+    define: {
+      window: '{}' // to be detected as ENVIRONMENT_IS_WEB with EmscriptenModule
+    }
   }
+
+  const mainEntry: Options = {
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    ...common
+  }
+  const workletEntries: Options = {
+    entry: [
+      'src/speex/workletProcessor.ts',
+      'src/noiseGate/workletProcessor.ts',
+      'src/rnnoise/workletProcessor.ts'
+    ],
+    ...common,
+    format: ['esm'],
+    minify: true,
+    splitting: false // to prevent "import" inside worklet which breaks vite
+  }
+
+  return [mainEntry, workletEntries]
 })
